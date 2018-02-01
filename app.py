591b8233b6061f0d385ffaa9f82b7f5fe31054f8
Jay <jay.cn@outlook.com>
2018-02-01 15:04:08 +0800

    framework for WIT web

--- app.py
+++ app.py
@@ -6,8 +6,28 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
 
-@app.route("/pages/index.html")
-def pages_index():
+@app.route("/record")
+def record():
+    return render_template("/pages/record.html")
+
+@app.route("/file_import")
+def file_import():
+    return render_template("/pages/file_import.html")
+
+@app.route("/index")
+def index():
     return render_template("/pages/index.html")
 
+
+@app.route("/jsw",methods=['GET','POST'])
+def jsw():
+    return "jsw"
+
+@app.route("/ditmco",methods=['GET','POST'])
+def ditmco():
+    return "pvg"
+
+@app.route("/clear",methods=['GET','POST'])
+def clear():
+    return "clear"
 app.run()
